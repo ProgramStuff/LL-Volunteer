2fec61381b0b9a053b2d576b08178eb8e97dfa28
ProgramStuff <jordanpkelsey1@gmail.com>
2024-10-09 10:09:32 -0300

    Revert "config file changes"
    This reverts commit f16811bccbeccf755d8cdcd3da46f2b709dcb9b9.

--- LL-Volunteer/vite.config.js
+++ LL-Volunteer/vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  return {
-    plugins: [react()],
-    build: {
-      outDir: 'dist',
-    },
-    define: {
-      __APP_ENV__: process.env.VITE_VERCEL_ENV, // Reference to the Vercel environment variable
-    },
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
+})
