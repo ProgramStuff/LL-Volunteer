4b09140afecd056ff0bb3bc786c5e82140b0d004
ProgramStuff <jordanpkelsey1@gmail.com>
2024-10-09 10:08:50 -0300

    Revert "update vercel config"
    This reverts commit 9ad5b136ee6a0523cb85185f9b10ad286e899b3c.

--- LL-Volunteer/src/server.js
+++ LL-Volunteer/src/server.js
@@ -16,8 +16,9 @@ const saltRounds = 10;
 env.config();
 const secret = process.env.SESSION_SECRET
 const port = process.env.PORT || 3000;
-const isProduction = process.env.NODE_ENV === 'production';
 
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 // Body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -85,7 +86,6 @@ const pool = createPool({
 // Helper function to handle database queries
 const queryDB = async (text, params) => {
   const client = await pool.connect();
-  console.log("CLIENT CONNECTED: " + client)
   try {
     const result = await client.query(text, params);
     return result;
